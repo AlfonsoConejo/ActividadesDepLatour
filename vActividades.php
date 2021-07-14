1219aa8fad556a01a362819c24b4ae53db8cedfc
AlfonsoConejo <49042515+AlfonsoConejo@users.noreply.github.com>
2021-07-13 19:10:43 -0500

    Add files via upload

---
+++ vActividades.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title> Actividades Deportivas </title>
+	<link rel="stylesheet" type="text/css" href="vActividades.css">
+</head>
+<body>
+	<header>
+		<div class="wrapper">
+			<div class="logo">
+				<a href="vActividades.php">CLUB LATOUR</a>
+			</div>
+
+			<nav>
+					<a href="#">Mi perfil</a>
+			</nav>
+		</div>
+	</header>
+
+	<div class="caja-actividades">
+			<h1>Actividades Culturales y Deportivas</h1>
+			<table class="default">
+			  <tr>
+			    <td class="ladoIzqTitle">Cultural</td>
+			    <td class="ladoDerTitle">Deportivo</td>
+			  </tr>
+			  <tr>
+			  <tr>
+			    <td class="ladoIzq">Ballet</td>
+			    <td class="ladoDer">Futbol</td>
+			  </tr>
+			  <tr>
+			    <td class="ladoIzqButton"> 
+			    	<a href="vBallet.php">Ver</a>
+			    </td>
+			    <td class="ladoDerButton">
+			    	<a href="vFutbol.php">Ver</a>
+			    </td>
+			  </tr>
+			   <tr>
+			    <td class="ladoIzq">Zumba</td>
+			    <td class="ladoDer">Tenis</td>
+			  </tr>
+			  <tr>
+			    <td class="ladoIzqButton"> 
+			    	<a href="zumba.php">Ver</a>
+			    </td>
+			    <td class="ladoDerButton">
+			    	<a href="vTenis.php">Ver</a>
+			    </td>
+			  </tr>
+			   <tr>
+			    <td class="ladoIzq">Yoga</td>
+			    <td class="ladoDer">Natación</td>
+			  </tr>
+			  <tr>
+			    <td class="ladoIzqButton"> 
+			    	<a href="vYoga.php">Ver</a>
+			    </td>
+			    <td class="ladoDerButton">
+			    	<a href="vNatacion.php">Ver</a>
+			    </td>
+			    </tr>
+			   <tr>
+			    <td class="ladoIzq">Danza</td>
+			    <td class="ladoDer">Voleibol</td>
+			  </tr>
+			  <tr>
+			    <td class="ladoIzqButton"> 
+			    	<a href="vDanza.php">Ver</a>
+			    </td>
+			    <td class="ladoDerButton">
+			    	<a href="vVoleibol.php">Ver</a>
+			    </td>
+			  </tr>
+			</table>
+		</div>
+</body>
+</html>
---
+++ zumba.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title> Información </title>
+	<link rel="stylesheet" type="text/css" href="vFutbol.css">
+</head>
+<body>
+	<header>
+		<div class="wrapper">
+			<div class="logo">
+				<a href="vActividades.php">CLUB LATOUR</a>
+			</div>
+
+			<nav>
+					<a href="#">Mi perfil</a>
+			</nav>
+		</div>
+	</header>
+
+	<div class="contenedor">
+		<div class="CajaTitulo">
+			<h1>Zumba</h1>
+		</div>
+		<div class="CajaHorario">
+			<p class="HorarioTitulo">Horarios</p>
+			<p class="TextoNegritas">Sábado:</p>
+			<p class="Texto">6:00 pm - 7:00 pm</p>
+			<p class="Texto">7:00 pm - 8:00 pm</p>
+			<p class="Texto">8:00 pm - 9:00 pm</p>
+			<p class="TextoNegritas">Domingo:</p>
+			<p class="Texto">6:00 pm - 7:00 pm</p>
+			<p class="Texto">7:00 pm - 8:00 pm</p>
+			<p class="Texto">8:00 pm - 9:00 pm</p>
+		</div>
+		<div class="CajaInformacion">
+			<p class="HorarioTitulo">Información</p>
+			<p class="TextoNegritas">Ubicación:</p>
+			<p class="Texto">Salón de baile</p>
+			<p class="TextoNegritas">Instructor:</p>
+			<p class="Texto">Luis Ángel Ramírez Flores</p>
+			<p class="TextoNegritas">Cupo mínimo:</p>
+			<p class="Texto">6</p>
+			<p class="TextoNegritas">Cupo máximo:</p>
+			<p class="Texto">25</p>
+		</div>
+		<div class="CajaImagen">
+			<div class="CajaImagenAnidada">
+				<img src="zumba.jpg">
+			</div>
+		</div>
+		<div class="CajaRegistro">
+			  <a href="###">Registrarse</a>
+		</div>
+		
+	</div>
+</body>
+</html>
